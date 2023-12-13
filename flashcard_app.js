a35004021c61d7df0068418e325c63cb2a72ae23
zhongzhihu <152866837+zhongzhihu@users.noreply.github.com>
2023-12-13 22:42:47 +0100

    Update flashcard_app.js

--- flashcard_app.js
+++ flashcard_app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,22 +6,22 @@ import {
   StyleSheet,
   TouchableOpacity,
   Image,
+  TextInput,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import * as Speech from "expo-speech";
 
-// I have more flashcards on my private environment
+// I have more flashcards in my private environment.
 const flashcards = [
   {
     id: 1,
     german: "das Huhn",
-    de_irr: "-ü-er",
+    de_irr: " ̈-er",
     de_sent: "Gestern haben wir das Huhn im Ofen gebacken.",
     ch: "das Poulet, -s",
     ch_sent: "Gestern haben wir das Poulet im Ofen gebacken.",
     english: "the chicken",
     en_sent: "Yesterday, we baked the chicken in the oven.",
-    points: 0,
   },
   {
     id: 2,
@@ -29,8 +29,7 @@ const flashcards = [
     de_irr: "isst, aß, hat gegessen",
     de_sent: "Er isst gerne Pommes mit Mayonnaise.",
     english: "to eat",
-    en_sent: "He likes to eat French fries with mayonnaise",
-    points: 0,
+    en_sent: "He likes to eat French fries with mayonnaise.",
   },
   {
     id: 3,
@@ -39,14 +38,35 @@ const flashcards = [
     de_sent: "Das Boot fährt auf dem Wasser.",
     english: "the boat",
     en_sent: "The boat is sailing on the water.",
-    points: 0,
   },
 ];
+
+const initialRatings = flashcards.map((card) => ({
+  id: card.id,
+  reviews: 0,
+  lastReviewed: null,
+  nextReview: null,
+}));
+
+const calculateNextReviewDate = (rating, lastReviewed) => {
+  const intervals = {
+    easy: 5, // days until next review if easy
+    correct: 3, // days if correct
+    hard: 1, // days if hard
+    fail: 0, // review again immediately if failed
+  };
+
+  const nextReviewDate = new Date(lastReviewed || new Date());
+  nextReviewDate.setDate(nextReviewDate.getDate() + intervals[rating]);
+  return nextReviewDate;
+};
 
 const FlashcardApp = () => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [showTranslation, setShowTranslation] = useState(false);
-  const [currentCardPoints, setCurrentCardPoints] = useState(0);
+  const [ratings, setRatings] = useState(initialRatings);
+  const [numCardsToReview, setNumCardsToReview] = useState(5);
+  const [startScreenVisible, setStartScreenVisible] = useState(true);
 
   const handleShowTranslation = () => {
     setShowTranslation(true);
@@ -57,113 +77,170 @@ const FlashcardApp = () => {
     await Speech.speak(germanWord, { language: "de" });
   };
 
-  const handleDifficulty = (points) => {
-    const updatedFlashcards = [...flashcards];
-    const updatedPoints = currentCardPoints + points;
-    updatedFlashcards[currentCardIndex].points = updatedPoints;
-    setCurrentCardPoints(updatedPoints);
-    handleNextCard();
-  };
-
-  const handleNextCard = () => {
-    const randomIndex = Math.floor(Math.random() * flashcards.length);
-    setCurrentCardIndex(randomIndex);
-    setShowTranslation(false);
-    setCurrentCardPoints(flashcards[randomIndex].points);
-  };
-
   const handleSentencePronunciation = async () => {
     const germanSentence = flashcards[currentCardIndex].de_sent;
     await Speech.speak(germanSentence, { language: "de" });
   };
 
+  const updateRating = (rating) => {
+    setRatings(
+      ratings.map((r) => {
+        if (r.id === flashcards[currentCardIndex].id) {
+          const newLastReviewed = new Date();
+          return {
+            ...r,
+            reviews: r.reviews + 1,
+            lastReviewed: newLastReviewed,
+            nextReview: calculateNextReviewDate(rating, newLastReviewed),
+          };
+        }
+        return r;
+      })
+    );
+
+    handleNextCard();
+  };
+
+  const handleNextCard = () => {
+    // Add logic to select the next card based on the nextReview dates
+    const sortedCards = ratings
+      .filter((r) => r.nextReview <= new Date())
+      .sort((a, b) => a.nextReview - b.nextReview);
+
+    const nextCardIndex =
+      sortedCards.length > 0
+        ? flashcards.findIndex((fc) => fc.id === sortedCards[0].id)
+        : Math.floor(Math.random() * flashcards.length);
+
+    setCurrentCardIndex(nextCardIndex);
+    setShowTranslation(false);
+  };
+
+  const handleStart = () => {
+    setStartScreenVisible(false);
+    // Additional logic for starting the review can be added here
+  };
+
+  if (startScreenVisible) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.input}>Enter the number of cards to review:</Text>
+        <TextInput
+          style={styles.input} //
+          onChangeText={(text) => {
+            // Check if the text is empty, and if so, set numCardsToReview to 0 or a default value
+            // to prevent NaN on screen
+            const num = text === "" ? 0 : parseInt(text, 10);
+            setNumCardsToReview(num);
+          }}
+          keyboardType="numeric"
+          defaultValue="5"
+          value={String(numCardsToReview)}
+        />
+        <Button title="Start Review" color="white" onPress={handleStart} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.pointCounter}>Points: {currentCardPoints}</Text>
-      </View>
-
-      <View style={styles.card}>
-        {!showTranslation && (
+      {!showTranslation && (
+        <View style={[styles.card, { backgroundColor: "#495a8d" }]}>
           <Text style={styles.cardText}>
             {flashcards[currentCardIndex].english}
           </Text>
-        )}
-
-        {showTranslation && (
-          <View>
-            {/* English Box */}
-            <View style={styles.translatedBox}>
-              <Text style={styles.cardText}>
-                {flashcards[currentCardIndex].english}
-              </Text>
-              <Text style={styles.sentenceText}>
-                {flashcards[currentCardIndex].en_sent}
-              </Text>
-            </View>
-
-            {/* Whitespace */}
-            <View style={{ height: 10 }} />
-
-            {/* German Box */}
-            <View style={styles.translatedBox}>
-              <Text style={styles.cardText}>
-                {flashcards[currentCardIndex].german}
-                {flashcards[currentCardIndex].de_irr
-                  ? `, ${flashcards[currentCardIndex].de_irr}`
-                  : ""}
-              </Text>
-              <Text style={styles.sentenceText}>
-                {flashcards[currentCardIndex].de_sent}
-              </Text>
-              <View style={styles.wordSpeakerContainer}>
-                <TouchableOpacity
-                  onPress={handlePronunciation}
-                  style={styles.wordSpeakerIcon}
-                >
-                  <Feather name="volume-2" size={24} color="black" />
-                </TouchableOpacity>
-              </View>
-              <View style={styles.sentenceSpeakerContainer}>
-                <TouchableOpacity onPress={handleSentencePronunciation}>
-                  <Feather name="volume-2" size={24} color="black" />
-                </TouchableOpacity>
-              </View>
-            </View>
-
-            {/* Whitespace */}
-            <View style={{ height: 10 }} />
-
-            {/* Schweizer Hochdeutsch Box*/}
-            {flashcards[currentCardIndex].ch && (
-              <View style={[styles.translatedBox, styles.swissBox]}>
-                <Image
-                  source={require("./swiss_flag.png")}
-                  style={styles.swissFlag}
-                />
-                <Text style={styles.cardText}>
-                  {flashcards[currentCardIndex].ch}
-                </Text>
-                <Text style={styles.sentenceText}>
-                  {flashcards[currentCardIndex].ch_sent}
-                </Text>
-              </View>
-            )}
-          </View>
-        )}
-      </View>
+        </View>
+      )}
 
       {showTranslation && (
-        <View style={styles.difficultyButtons}>
-          <Button title="Easy" onPress={() => handleDifficulty(5)} />
-          <Button title="Correct" onPress={() => handleDifficulty(1)} />
-          <Button title="Hard" onPress={() => handleDifficulty(-2)} />
-          <Button title="Fail" onPress={() => handleDifficulty(-5)} />
+        <View>
+          {/* English Box */}
+          <View style={[styles.card, { backgroundColor: "#495a8d" }]}>
+            <Text style={styles.cardText}>
+              {flashcards[currentCardIndex].english}
+            </Text>
+            <Text style={styles.sentenceText}>
+              {flashcards[currentCardIndex].en_sent}
+            </Text>
+          </View>
+
+          {/* Whitespace */}
+          <View style={{ height: 10 }} />
+
+          {/* German Box */}
+          <View style={styles.card}>
+            <Text style={styles.cardText}>
+              {flashcards[currentCardIndex].german}
+              {flashcards[currentCardIndex].de_irr
+                ? `, ${flashcards[currentCardIndex].de_irr}`
+                : ""}
+            </Text>
+            <Text style={styles.sentenceText}>
+              {flashcards[currentCardIndex].de_sent}
+            </Text>
+            <View style={styles.wordSpeakerContainer}>
+              <TouchableOpacity
+                onPress={handlePronunciation}
+                style={styles.wordSpeakerIcon}
+              >
+                <Feather name="volume-2" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sentenceSpeakerContainer}>
+              <TouchableOpacity onPress={handleSentencePronunciation}>
+                <Feather name="volume-2" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* Whitespace */}
+          <View style={{ height: 10 }} />
+
+          {/* Schweizer Hochdeutsch Box */}
+          {flashcards[currentCardIndex].ch && (
+            <View style={styles.card}>
+              <Image
+                source={require("./swiss_flag.png")}
+                style={styles.swissFlag}
+              />
+              <Text style={styles.cardText}>
+                {flashcards[currentCardIndex].ch}
+              </Text>
+              <Text style={styles.sentenceText}>
+                {flashcards[currentCardIndex].ch_sent}
+              </Text>
+            </View>
+          )}
         </View>
       )}
 
       {!showTranslation && (
-        <Button title="Flip" onPress={handleShowTranslation} />
+        <Button title="Flip" color="white" onPress={handleShowTranslation} />
+      )}
+
+      {showTranslation && (
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Easy"
+            color="white"
+            onPress={() => updateRating("easy")}
+          />
+          <Button
+            title="Correct"
+            color="white"
+            onPress={() => updateRating("correct")}
+          />
+          <Button
+            title="Hard"
+            color="white"
+            onPress={() => updateRating("hard")}
+          />
+          <Button
+            title="Fail"
+            color="white"
+            onPress={() => updateRating("fail")}
+          />
+        </View>
       )}
     </View>
   );
@@ -174,52 +251,34 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
-  },
-  header: {
-    position: "absolute",
-    top: 10,
-    right: 10,
-  },
-  pointCounter: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  card: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    marginBottom: 30,
+    backgroundColor: "#191c53",
   },
   cardText: {
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
+    color: "white",
+    marginLeft: 20,
+    marginRight: 20,
   },
   sentenceText: {
     fontSize: 14,
     textAlign: "center",
     marginTop: 10,
     marginBottom: 20,
+    color: "white",
   },
-  translatedBox: {
-    backgroundColor: "lightblue",
+  card: {
+    backgroundColor: "#4d64ff",
     padding: 20,
     borderRadius: 10,
     marginBottom: 20,
     position: "relative",
   },
-  difficultyButtons: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "80%",
-    position: "absolute",
-    bottom: 50,
-  },
   wordSpeakerContainer: {
     position: "absolute",
-    marginTop: 15,
-    right: 5,
+    marginTop: 13,
+    right: 10,
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
@@ -233,13 +292,28 @@ const styles = StyleSheet.create({
     marginRight: 10,
     marginTop: 10,
   },
-  swissBox: {
-    backgroundColor: "lightblue",
-  },
   swissFlag: {
     width: 20,
     height: 20,
+    position: "absolute",
+    marginTop: 25,
+    marginLeft: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+    position: "absolute",
+    bottom: 50,
+  },
+  input: {
+    fontSize: 20,
+    marginTop: 5,
+    marginBottom: 20,
+    color: "white",
   },
 });
 
 export default FlashcardApp;
+
+
